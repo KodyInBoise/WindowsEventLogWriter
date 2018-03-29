0eb94cc117e83495ce8b76660bd59fa70def346e
Kody Kriner <kody.kriner@gmail.com>
2018-03-28 19:08:38 -0600

    Simplified code

--- EventLogWriter/Program.cs
+++ EventLogWriter/Program.cs
@@ -16,23 +16,10 @@ namespace EventLogWriter
             {
                 var parameters = ParseArguments(args);
 
-                var _source = parameters.Find(s => s.ToLower().Contains("source:"));
-                var _log = parameters.Find(l => l.ToLower().Contains("log:"));
-                var _event = parameters.Find(e => e.ToLower().Contains("event:"));
-                var _message = parameters.Find(m => m.Contains("message:") || m.Contains("Message:"));
-
-                //Scrub paramneters or set defaults
-                if (!String.IsNullOrWhiteSpace(_source)) _source = ScrubParam(_source);
-                else _source = "Event Log Writer";
-
-                if (!String.IsNullOrWhiteSpace(_log)) _log = ScrubParam(_log);
-                else _log = "Application";
-
-                if (!String.IsNullOrWhiteSpace(_event)) _event = ScrubParam(_event);
-                else _event = "Wrote to Windows Event Log";
-
-                if (!String.IsNullOrWhiteSpace(_message)) _message = ScrubParam(_message);
-                else _message = "Empty message";
+                var _source = ScrubParam(parameters.Find(s => s.ToLower().Contains("source:"))) ?? "Event Log Writer";
+                var _log = ScrubParam(parameters.Find(l => l.ToLower().Contains("log:"))) ?? "Application";
+                var _event = ScrubParam(parameters.Find(e => e.ToLower().Contains("event:"))) ?? "Wrote to Windows Event Log";
+                var _message = ScrubParam(parameters.Find(m => m.Contains("message:") || m.Contains("Message:"))) ?? "Empty Message";
 
                 //Write to Windows event log
                 if (!EventLog.SourceExists(_source)) EventLog.CreateEventSource(_source, _log);
